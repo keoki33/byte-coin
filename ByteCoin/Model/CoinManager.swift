310a7219621d95d859ad21008a5fa8fdb5a6c112
keoki33 <gwong323@yahoo.com>
2020-04-14 13:34:18 +1000

    fetch and print exchange rate challenge

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -11,13 +11,42 @@ import Foundation
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "F488DB23-FC52-4FBA-B3BB-0FC6542D3CE9"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
     
     func getCoinPrice(for currency: String) -> Void {
-         print(currency)
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        performRequest(with: urlString)
     }
     
+    
+  
+    
+    
+    func performRequest(with urlString: String) -> Void {
+        print(urlString)
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+//                    self.delegate?.didFailWithError(error: error!)
+                    print(error!)
+                    return
+                }
+                if let safeData = data {
+//                    if let exchangeRate = self.parseJson(safeData) {
+                    print(String(data: safeData, encoding: String.Encoding.utf8) as String? as Any)
+                    }
+                   
+                
+                }
+            task.resume()
+            }
+        }
+    
+    
+    
+//    var backToString = String(data: somedata!, encoding: String.Encoding.utf8) as String!
 }
